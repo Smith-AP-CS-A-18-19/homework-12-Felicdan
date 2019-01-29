6ecdc70d78713e949b3041f11456f9197ab908c2
Daniel Feliciano <felicdan@berkeleyprep.org>
2019-01-29 16:26:10 -0500

    heyo

--- Homework12.java
+++ Homework12.java
@@ -4,21 +4,24 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RectangularShape;
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class Homework12 {
 
-	/* Finish the constructor and create any necessary instance
+	/* Finish the constructor and create any necessary instance-
 	 * variables. The constructor should create and save an
 	 * ArrayList of RectangularShape values
 	 */
-	public Homework12() {
+	 private ArrayList<RectangularShape> shapes;
 
+	public Homework12() {
+		shapes = new ArrayList<RectangularShape>();
 	}
 
 	/* Adds the parameter s to the ArrayList
 	 */
 	public void addShape(RectangularShape s) {
-
+		shapes.add(s);
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -26,7 +29,13 @@ public class Homework12 {
 	 * parameter r
 	 */
 	public int problem1(Rectangle2D r) {
-
+		int numRects = 0;
+		for(RectangularShape b : shapes){
+			if(b.intersects(r)){
+				numRects++;
+			}
+		}
+		return numRects;
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -34,21 +43,40 @@ public class Homework12 {
 	 * parameter p
 	 */
 	public int problem2(Point2D p) {
-
+		int numRects = 0;
+		for(RectangularShape b : shapes){
+			if(b.contains(p) == false){
+				numRects++;
+			}
+		}
+		return numRects;
 	}
 
 	/* Returns the number of Ellipse2D objects that
 	 * are in the ArrayList
 	 */
 	public int problem3() {
-
+		int numEllip = 0;
+		for(RectangularShape i : shapes){
+			if(i instanceof Ellipse2D){
+				numEllip++;
+			}
+		}
+		return numEllip;
 	}
 
 	/* Returns true if any of the Rectangle2D objects
 	 * in the ArrayList intersects the parameter Line2D
 	 */
 	public boolean problem4(Line2D l) {
-
+		for(RectangularShape i : shapes){
+			if(i instanceof Rectangle2D){
+				if(((Rectangle2D)(i)).intersectsLine(l)){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	/* Return the total area of the RectangularShape objects in
@@ -60,7 +88,18 @@ public class Homework12 {
 	 * counts for both (i.e. do not account for intersection)
 	 */
 	public double problem5() {
-
+		double totArea = 0;
+		for(RectangularShape r : shapes){
+			if(r instanceof Rectangle2D){
+				double area = ((Rectangle2D)(r)).getWidth() * ((Rectangle2D)(r)).getHeight();
+				totArea += area;
+			}
+			if(r instanceof Ellipse2D) {
+				double area = Math.PI * Math.pow(((((Ellipse2D)(r)).getWidth())/2), 2) ;
+				totArea += area;
+			}
+		}
+		return totArea;
 	}
 
 	public static void main(String[] args) {
